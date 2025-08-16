8de6e152e75266bf1d6f5df9bfa3fb60ac519275
jettieb <bokjihee@naver.com>
2025-08-16 15:31:07 +0900

    ✨ feat: 카카오톡 로그인 연결

---
+++ salgulok/src/pages/auth/KakaoRedirectPage.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { sendKakaoCode } from "../../api/auth";
+import { useNavigate } from "react-router-dom";
+
+function KakaoRedirectPage() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const code = new URL(window.location.href).searchParams.get("code");
+    if (!code) return;
+
+    sendKakaoCode(code)
+      .then((res) => {
+        console.log("로그인 성공", res); // TODO: 배포시에는 콘솔로그 제거
+
+        if (res.newUser) {
+          navigate("/signup"); // 추가 회원정보 입력 페이지
+        } else {
+          navigate("/"); // 메인 페이지
+        }
+      })
+      .catch((err) => {
+        console.error("로그인 실패", err);
+        alert("로그인 중 문제가 발생했습니다.");
+        navigate("/login");
+      });
+  }, []);
+
+  return <div>로그인 처리 중...</div>;
+}
+
+export default KakaoRedirectPage;
---
+++ salgulok/src/pages/auth/SignupPage.tsx
@@ -0,0 +1,9 @@
+function SignupPage() {
+  return (
+    <div>
+      <h1>회원정보 추가 입력 페이지</h1>
+    </div>
+  );
+}
+
+export default SignupPage;

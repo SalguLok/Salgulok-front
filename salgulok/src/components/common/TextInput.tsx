59bfd868a3395acf578cee17074c9acf0dcb22b5
jettieb <bokjihee@naver.com>
2025-09-18 02:18:30 +0900

    ✨ textInput에 입력했으면 글씨 검은색으로 고정

--- salgulok/src/components/common/TextInput.tsx
+++ salgulok/src/components/common/TextInput.tsx
@@ -3,33 +3,35 @@ import styled from "styled-components";
 
 interface TextAreaProps extends React.InputHTMLAttributes<HTMLTextAreaElement> {
   variant?: "sm" | "md" | "lg";
+  value?: string;
 }
-
-const TextInput: React.FC<TextAreaProps> = ({ variant = "md", ...props }) => {
-  return <StyledTextarea $variant={variant} {...props} />;
-};
-
-export default TextInput;
 
 const sizeMap = {
   sm: { height: "36px" },
   md: { height: "100px" },
   lg: { height: "196px" },
+} as const;
+
+const TextInput: React.FC<TextAreaProps> = ({ variant = "md", value, ...props }) => {
+  const hasValue = Boolean(value && value.length > 0);
+
+  return <StyledTextarea $variant={variant} $hasValue={hasValue} value={value} {...props} />;
 };
 
-const StyledTextarea = styled.textarea<{ $variant?: "sm" | "md" | "lg" }>`
+export default TextInput;
+
+const StyledTextarea = styled.textarea<{ $variant?: "sm" | "md" | "lg"; $hasValue: boolean }>`
   border: 1.5px solid var(--gray-300);
-  color: var(--gray-300);
   border-radius: 10px;
   flex: 1;
-  height: ${(props) => sizeMap[props.$variant || "sm"].height};
+  height: ${(props) => sizeMap[props.$variant ?? "sm"].height};
   resize: none;
   padding: 12px 14px;
   font-size: 13px;
   line-height: 13px;
-  align-items: center; 
   font-weight: 500;
   box-sizing: border-box;
+  color: ${(props) => (props.$hasValue ? "var(--black)" : "var(--gray-300)")};
 
   &:focus {
     outline: none;
--- salgulok/src/components/common/TextInputSmall.tsx
+++ salgulok/src/components/common/TextInputSmall.tsx
@@ -1,25 +1,29 @@
 import React from "react";
 import styled from "styled-components";
 
-interface TextInputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+interface TextInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  value?: string;
+}
 
-const TextInput: React.FC<TextInputProps> = (props) => {
-  return <StyledInput {...props} />;
+const TextInput: React.FC<TextInputProps> = ({ value, ...props }) => {
+  const hasValue = Boolean(value && value.length > 0);
+  return <StyledInput $hasValue={hasValue} value={value} {...props} />;
 };
 
 export default TextInput;
 
-const StyledInput = styled.input`
+const StyledInput = styled.input<{ $hasValue: boolean }>`
   border: 1.5px solid var(--gray-300);
-  color: var(--gray-300);
   border-radius: 10px;
   height: 36px;
   padding: 0 14px;
   font-size: 13px;
   line-height: 20px;
   font-weight: 500;
-  flex:1;
+  flex: 1;
   box-sizing: border-box;
+
+  color: ${(props) => (props.$hasValue ? "var(--black)" : "var(--gray-300)")};
 
   &:focus {
     outline: none;

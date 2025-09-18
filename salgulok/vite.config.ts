b0261a9a987fe89916b41f8256ebd228f1fe0e3f
junexmun <angkmfirefoxygal@sookmyung.ac.kr>
2025-09-18 20:23:43 +0900

    salgulok vite.config.ts 수정

--- salgulok/vite.config.ts
+++ salgulok/vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    }, // alias 객체 닫기
-  }, // resolve 객체 닫기
   server: {
     proxy: {
       // 커뮤니티 API만 로컬 백엔드로 프록시

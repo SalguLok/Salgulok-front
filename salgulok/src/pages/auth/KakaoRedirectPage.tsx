97a70ec2961ba33716493c01a0b52e62182b847d
jettieb <bokjihee@naver.com>
2025-09-23 10:10:48 +0900

    💄 ui: 로그인 로딩화면 수정

--- salgulok/src/pages/auth/KakaoRedirectPage.tsx
+++ salgulok/src/pages/auth/KakaoRedirectPage.tsx
@@ -1,9 +1,10 @@
 import { useEffect } from "react";
 import { sendKakaoCode } from "../../api/auth/login";
 import { useNavigate } from "react-router-dom";
+import styled, { keyframes } from "styled-components";
 
 function KakaoRedirectPage() {
-  localStorage.removeItem("accessToken"); //헤더의 accessToken 지우기
+  localStorage.removeItem("accessToken"); // 헤더의 accessToken 지우기
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -27,7 +28,42 @@ function KakaoRedirectPage() {
       });
   }, []);
 
-  return <div>로그인 처리 중...</div>;
+  return (
+    <Wrapper>
+      <Spinner />
+      <Text>살구록 로그인 중...</Text>
+    </Wrapper>
+  );
 }
 
 export default KakaoRedirectPage;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background: var(--main-sec);
+`;
+
+const spin = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`;
+
+const Spinner = styled.div`
+  border: 6px solid var(--main-sec);
+  border-top: 6px solid var(--main-pri);
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: ${spin} 1s linear infinite;
+  margin-bottom: 16px;
+`;
+
+const Text = styled.p`
+  font-size: 16px;
+  font-weight: bold;
+  color: var(--main-pri);
+`;

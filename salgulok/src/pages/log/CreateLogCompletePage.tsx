84dfb951371b7c04b4b17e7c90fd12f5c0fe3a7f
sj0919 <seungjinlee09@gmail.com>
2025-09-25 17:19:53 +0900

    ♻️refactor : 로그 생성 완료 후 이동하는 경로 수정

--- salgulok/src/pages/log/CreateLogCompletePage.tsx
+++ salgulok/src/pages/log/CreateLogCompletePage.tsx
@@ -1,41 +1,37 @@
 import styled from "styled-components";
 import { useLocation, useNavigate, Link } from "react-router-dom";
-import LogoImg from "../../assets/common/main_salgu.svg"
+import LogoImg from "../../assets/common/main_salgu.svg";
 import BottomButton from "../../components/common/BottomButton";
 import Header from "../../components/common/Header";
 
 const CreateLogCompletePage: React.FC = () => {
+  const location = useLocation();
+  const logId = location.state?.logId;
+  const navigate = useNavigate();
 
-    const location = useLocation();
-    const logId = location.state?.logId;
-    const navigate = useNavigate();
-
-    const handleGoLog = () => {
-       if (logId) {
-        navigate(`/log/${logId}`);
-      } else {
-        navigate("/");
-      }
+  const handleGoLog = () => {
+    if (logId) {
+      navigate(`/log/${logId}/entries`);
+    } else {
+      navigate("/");
     }
+  };
 
-    return (
-      <Container>
-        <Header title="살구로그 생성"/>
+  return (
+    <Container>
+      <Header title="살구로그 생성" />
 
-        <ContentWrapper>
-          <Logo src={LogoImg}/>
-          <MainText>로그가 생성되었어요!</MainText>
-          <SubText>소중한 순간을 로그에 남겨주세요.</SubText>
-        </ContentWrapper>
+      <ContentWrapper>
+        <Logo src={LogoImg} />
+        <MainText>로그가 생성되었어요!</MainText>
+        <SubText>소중한 순간을 로그에 남겨주세요.</SubText>
+      </ContentWrapper>
 
-        <HomeLink to="/">홈으로 가기</HomeLink>
-        <BottomButton
-          text="로그 보러가기"
-          onClick={handleGoLog}
-        />
-      </Container>
-    );
-}
+      <HomeLink to="/">홈으로 가기</HomeLink>
+      <BottomButton text="로그 보러가기" onClick={handleGoLog} />
+    </Container>
+  );
+};
 
 export default CreateLogCompletePage;
 
